3285242bcd8e96cf01878753810d6d1dd9914688
mayur-gupta-smartsense <mayurg729@gmail.com>
2024-09-04 10:45:46 +0530

    new question added

--- async-await.js
+++ async-await.js
@@ -14,3 +14,42 @@ console.log(3);
 check();
 console.log(4);
 //  Answer: 3142
+
+// #jobs1
+const job1 = (cb) => {
+	setTimeout(() => {
+		cb("first");
+	}, 500);
+};
+const job2 = (cb) => {
+	setTimeout(() => {
+		cb("second");
+	}, 30);
+};
+const job3 = (cb) => {
+	setTimeout(() => {
+		cb("third");
+	}, 900);
+};
+
+const jobs = [job1, job2, job3];
+/**
+Each of these jobs use setTimeout to simulate a long-running task.
+Based on the code above, job2 will return first, followed by job1
+and finally, job3 will return.
+ *
+Write a function called `asyncMap` that passes an array of job results
+(in the same order in which they were originally passed in) to the final
+callback e.g. ['first', 'second', 'third']
+ */
+
+//Answer:
+
+const asyncMap = async (jobs, done) => {
+	const promises = jobs.map((job) => new Promise((resolve) => job(resolve)));
+	const results = await Promise.all(promises);
+	done(results);
+};
+asyncMap(jobs, (results) => {
+	console.log("pointer-->", results); // ['first', 'second', 'third']
+});
