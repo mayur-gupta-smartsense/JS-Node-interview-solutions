08b2d749634d57d560a5240373858ba6c59dfdb5
Mayur Gupta <mayurg729@gmail.com>
2025-11-23 11:34:17 +0530

    The ?? operator

--- javascript.js
+++ javascript.js
@@ -12,7 +12,34 @@ function sample() {
 }
 sample();
 console.log("a2", a);
+
+
+
 //Ans→ a1, undefined \n a2, 10
+
+console.log(false ?? 17) // fasle
+console.log(null ?? 18)  // 18
+console.log(undefined ?? 19) // 19
+
+// The ?? operator is similiar to || operator
+
+/*
+  The || operator:
+
+Returns the right side if the left side is any falsy value
+Falsy values: false, 0, '', null, undefined, NaN
+
+The ?? operator:
+
+Returns the right side only if the left side is null or undefined
+
+
+
+
+
+**/
 
 // I am just testing Git from  this useless commit
 // I am just again testing this Git from my local machine.
+
+
